556e9a64da8b5cf207bc26f46dbee00ede74e2df
PremSupasrisan <139094326+PremSupasrisan@users.noreply.github.com>
2023-07-10 10:38:36 +0700

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<" Student code : 66-060216-3007-0"<<endl;
+	cout<<" Student Name : Prem Supasrisan"<<endl;
+	cout<<"     Division : Information Tecnology"<<endl;
+	cout<<"         Year : 18 "<<endl;
+	cout<<"         Room : RC"<<endl;
+	cout<<"          Sex : Male"<<endl;
+	cout<<"          Age : 18"<<endl;
+	system("pause");
+	return 0;
+}
